31d55a6ba857740d5ece43dd0a527c2ea9248787
Randy May <wrandymay@gmail.com>
2019-11-27 07:37:11 -0500

    saving work

---
+++ jet-server/src/main/java/com/hazelcast/training/streams/server/Server.java
@@ -0,0 +1,11 @@
+package com.hazelcast.training.streams.server;
+
+import com.hazelcast.jet.Jet;
+import com.hazelcast.jet.JetInstance;
+
+public class Server
+{
+    public static void main( String[] args ) {
+        JetInstance jet = Jet.newJetInstance();
+    }
+}
