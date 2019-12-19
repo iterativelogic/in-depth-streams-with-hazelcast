5f274a5de5e911046616951de578377a5d4467eb
Randy May <wrandymay@gmail.com>
2019-12-19 16:38:30 -0500

    Update the dashboard to show crashes.

--- UI/dashboard.py
+++ UI/dashboard.py
@@ -24,9 +24,9 @@ hz_config.network_config.connection_attempt_period = 5
 hz = hazelcast.HazelcastClient(hz_config)
 
 # position map and lock
-position_map = hz.get_map('vehicles')  # remote hz map
-position_change_map_lock = threading.Lock()  # if we eventually end up with multiple map servers this should be a distributed lock
-position_change_map = dict()  # changes accumulate in this local map
+vehicle_map = hz.get_map('vehicles')  # remote hz map
+vehicle_map_lock = threading.Lock()  # if we eventually end up with multiple map servers this should be a distributed lock
+change_map = dict()  # changes accumulate in this local map
 
 
 # color map and lock
@@ -40,33 +40,42 @@ position_change_map = dict()  # changes accumulate in this local map
 
 
 # add a listener to position map to place all add/update events into the changeMap
-def position_listener(event):
-    global position_change_map
-    with position_change_map_lock:
-        position_change_map[event.key] = event.value
+def vehicle_map_listener(event):
+    global change_map
+    with vehicle_map_lock:
+        change_map[event.key] = event.value
 
 
-position_map.add_entry_listener(include_value=True, updated_func=position_listener, added_func=position_listener)
+vehicle_map.add_entry_listener(include_value=True, updated_func=vehicle_map_listener, added_func=vehicle_map_listener)
 
-# add a listener to position map to place all add/update events into the changeMap
-# def color_listener(event):
-#     global color_change_map
-#     with color_change_map_lock:
-#         color_change_map[event.key] = event.value
-#
-#
-# color_map.add_entry_listener(include_value=True, added_func=color_listener, updated_func=color_listener)
+
+def colormap(entry):
+    if 'status' in entry and entry['status'] == 'CRASHED':
+        return 'red'
+    else:
+        return 'blue'
+
+
+def sizemap(entry):
+    if 'status' in entry and entry['status'] == 'CRASHED':
+        return 10
+    else:
+        return 6
+
 
 # now retrieve all entries from the map and build a ColumnDataSource
 
 # apparently map.values() returns a concurrent.futures.Future
-values = [entry.loads() for entry in position_map.values().result()]
+values = [entry.loads() for entry in vehicle_map.values().result()]
 latitudes = [entry['latitude'] for entry in values]
 longitudes = [entry['longitude'] for entry in values]
-id_to_index_map = { item['vin'] : i for (i, item) in enumerate(values)}
-# colors = ['gray' for c in range(len(latitudes))]
-# alphas = [1.0 for a in range(len(latitudes))]
-data_source = ColumnDataSource({'latitude': latitudes, 'longitude': longitudes})
+colors = [colormap(entry) for entry in values]
+sizes = [sizemap(entry) for entry in values]
+
+data_source = ColumnDataSource({'latitude': latitudes, 'longitude': longitudes, 'color': colors, 'size': sizes})
+
+# map each vin to a certain position within the lists
+id_to_index_map = {item['vin']: i for (i, item) in enumerate(values)}
 
 # build the map
 louisville_lat = 38 + 15 / 60 + 9 / 3600
@@ -74,37 +83,27 @@ louisville_long = -1 * (85 + 18 / 60 + 41 / 3600)
 
 map_options = GMapOptions(map_type='roadmap', lat=louisville_lat, lng=louisville_long, zoom=6)
 p = gmap("AIzaSyDiRy33QAJj4gMedAs8Cde6e3-bER68Htw", map_options, title='US')
-p.circle(x='longitude', y='latitude', color='blue', size=6, source=data_source)
+p.circle(x='longitude', y='latitude', color='color', size='size', source=data_source)
 layout = column(p)
 
 
 def update():
     patches = dict()
-    if len(position_change_map) > 0:
-        with position_change_map_lock:
-            entry_list = [entry for entry in [e.loads() for e in position_change_map.values()] if
+    if len(change_map) > 0:
+        with vehicle_map_lock:
+            entry_list = [entry for entry in [e.loads() for e in change_map.values()] if
                           entry['vin'] in id_to_index_map]  # in check is costly, can we do something better ?
             longitude_patches = [(id_to_index_map[entry['vin']], entry['longitude']) for entry in entry_list]
             latitude_patches = [(id_to_index_map[entry['vin']], entry['latitude']) for entry in entry_list]
+            color_patches = [(id_to_index_map[entry['vin']], colormap(entry)) for entry in entry_list]
+            size_patches = [(id_to_index_map[entry['vin']], sizemap(entry)) for entry in entry_list]
             patches['longitude'] = longitude_patches
             patches['latitude'] = latitude_patches
-            position_change_map.clear()
+            patches['size'] = size_patches
+            patches['color'] = color_patches
+            change_map.clear()
     else:
         logging.debug('no position changes')
-
-    # if len(color_change_map) > 0:
-    #     with color_change_map_lock:
-    #         color_patches = [(k, v) for k, v in color_change_map.items() if k in ids]  # in check is costly, can we do something better ?
-    #         color_change_map.clear()
-    #
-    #     patches['color'] = color_patches
-    #
-    # if len(alpha_change_map) > 0:
-    #     with alpha_change_map_lock:
-    #         alpha_patches = [ (k,v) for k,v in alpha_change_map.items() if k in ids]
-    #         alpha_change_map.clear()
-    #
-    #     patches['alpha'] = alpha_patches
 
     if len(patches) > 0:
         data_source.patch(patches)
